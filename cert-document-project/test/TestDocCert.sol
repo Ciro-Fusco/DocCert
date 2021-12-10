5fcf87217ad88083da5616385fae34fa9e66abf3
Vincenzo Aiello <vincenzoaiello300@gmail.com>
2021-12-10 17:08:20 +0100

    Fine test

--- cert-document-project/test/TestDocCert.sol
+++ cert-document-project/test/TestDocCert.sol
@@ -14,6 +14,8 @@ contract TestDocCert{
 
  //The expected owner of the document is this contract
  address[] expectedOwner;
+
+
  // Testing the addFile() function
  function testDocumentCanBeAdded() public {
    //Adding first owner to array
@@ -22,17 +24,50 @@ contract TestDocCert{
    doc.addFile(test_hash, test_cid);
 
  }
+
+  function testDocumentCanNotBeAdded() public {
+    
+  try doc.addFile(test_hash, test_cid) 
+  {
+    Assert.isTrue(false,"Test aggiunta documento seconda volta fallito");
+  }
+  catch Error(string memory e)
+  {
+    Assert.isTrue(true,"");
+  }
+
+ }
+
+
  // Testing Verifica of a document
  function testVerifica() public {
    (address[] memory a, , ) = doc.verifica(test_hash);
  
    Assert.equal(a, expectedOwner, "Owners should be equal but are not!");
+  }
+
+  
+  function testVerificaFalseDoc() public {
+   try doc.verifica("fab001") returns (address[] memory a, uint b , uint c )
+   {
+     Assert.isTrue(false,"Verifica non fallita correttamente");
+   }
+   catch Error(string memory e)
+   {
+     Assert.isTrue(true,"");
+   }
+ 
     }
+
+
  // Testing setOwner of a Document
  function testSetOwner() public {
    expectedOwner.push(address(0x00000000000000000002113400000000));
    doc.setOwner(test_hash,address(0x00000000000000000002113400000000));
-   
+
+   (address[] memory a, , ) = doc.verifica(test_hash);
+   Assert.equal(a, expectedOwner, "Owners should be equal but are not!");
+
   }
   
   //Testing getCid of a Document
